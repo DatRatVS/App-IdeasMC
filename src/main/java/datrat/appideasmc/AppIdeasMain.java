c26823fc466428885c5ce4a6b038102e1d86fdba
DatRatVS <ricorian2006@gmail.com>
2021-06-01 20:03:55 -0300

    #1 - Preparation

---
+++ src/main/java/datrat/appideasmc/AppIdeasMain.java
@@ -0,0 +1,14 @@
+package datrat.appideasmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AppIdeasMain extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
